6b0225f3044dc84d277ddc97d55f7cd795c93ed0
agnesiayolanda <117900996+agnesiayolandaaa@users.noreply.github.com>
2023-02-22 18:40:20 +0700

    Delete 1_D_71220820.py

--- 1_D_71220820.py
+++
@@ -1,3 +0,0 @@
-curah_hujan = int(input("Masukkan nilai curah hujan:"))
-kondisi_hujan = "Cuaca terang/berawan" if curah_hujan > 0 and curah_hujan <= 20 else "Curah hujan ringan" if curah_hujan > 20 and curah_hujan <= 50 else "Curah hujan sedang" if curah_hujan > 50 and curah_hujan <= 100 else "Curah hujan lebat" if curah_hujan > 100 else "Curah hujan ekstrem"
-print(kondisi_hujan)
